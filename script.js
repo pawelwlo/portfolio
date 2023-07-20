9b4a60d413c8746f33c1875f8bcc6ad977764a73
Pawel <113646840+pawelwlo@users.noreply.github.com>
2023-07-20 02:31:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,90 @@
-let slides = [...document.getElementsByClassName("slider__slide")];
-let navlinks = document.getElementsByClassName("slider__navlink");
 
-let currentSlide = 0;
 
-document.getElementById("nav-button--next").addEventListener("click", () => {
-    changeSlide(currentSlide + 1);
-});
+// const header = document.querySelector(".header");
+// const btns = header.querySelectorAll(".btn_1, .btn_2, .btn_3, .btn_4 ");
 
-document.getElementById("nav-button--prev").addEventListener("click", () => {
-    changeSlide(currentSlide - 1);
-});
 
-function changeSlide(moveTo) {
-    if (moveTo >= slides.length) {
-        moveTo = 0;
-    }
-    if (moveTo < 0) {
-        moveTo = slides.length - 1;
-    }
 
-    slides[currentSlide].classList.toggle("active");
-    navlinks[currentSlide].classList.toggle("active");
-    slides[moveTo].classList.toggle("active");
-    navlinks[moveTo].classList.toggle("active");
+// for (const btn of btns ) {
+    
+        
+//         btn.addEventListener("click", function changeBackground(e) {
+//             console.log(e);
+//             e.preventDefault();
+//             btn.classList.toggle("active");
+            
+//         });
+        
+// }
 
-    currentSlide = moveTo;
+var header = document.querySelector(".header");
+var btns = header.querySelectorAll(".btn_1, .btn_2, .btn_3, .btn_4 ");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
 }
 
-document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
-    bullet.addEventListener('click', () => {
-        if (currentSlide !== bulletIndex) {
-            changeSlide(bulletIndex);
-        }
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let slides = [...document.getElementsByClassName("slider__slide")];
+// let navlinks = document.getElementsByClassName("slider__navlink");
+
+// let currentSlide = 0;
+
+// document.getElementById("nav-button--next").addEventListener("click", () => {
+//     changeSlide(currentSlide + 1);
+// });
+
+// document.getElementById("nav-button--prev").addEventListener("click", () => {
+//     changeSlide(currentSlide - 1);
+// });
+
+// function changeSlide(moveTo) {
+//     if (moveTo >= slides.length) {
+//         moveTo = 0;
+//     }
+//     if (moveTo < 0) {
+//         moveTo = slides.length - 1;
+//     }
+
+//     slides[currentSlide].classList.toggle("active");
+//     navlinks[currentSlide].classList.toggle("active");
+//     slides[moveTo].classList.toggle("active");
+//     navlinks[moveTo].classList.toggle("active");
+
+//     currentSlide = moveTo;
+// }
+
+// document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+//     bullet.addEventListener('click', () => {
+//         if (currentSlide !== bulletIndex) {
+//             changeSlide(bulletIndex);
+//         }
+//     });
+// });
 
 
 // let slideLinks = [
